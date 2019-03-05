df7045c7ddef981e214827b4f5856fb2e4d1975b
JupiterPi <jupiterpi@outlook.de>
2019-03-05 13:55:00 +0100

    Initial Commit Personal

---
+++ src/main/java/jupiterpi/monpolyscoresaver/MonopolyScoreSaver.java
@@ -0,0 +1,9 @@
+package jupiterpi.monopolyscoresaver;
+
+public class MonopolyScoreSaver
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("APPLICATION MONPOLYSCORESAVER STARTED");
+    }
+}
